d3e56d9bbc1a3abe6a68715be4ce94711271364c
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2016-10-16 20:25:01 +0300

    Added polling input listener test.

--- algostorm-engine/src/main/kotlin/com/aheidelbacher/algostorm/engine/input/InputListener.kt
+++ algostorm-engine/src/main/kotlin/com/aheidelbacher/algostorm/engine/input/InputListener.kt
@@ -8,6 +8,9 @@ package com.aheidelbacher.algostorm.engine.input
  *
  * It is recommended to not access or modify game state, but simply to map raw
  * user input to user actions and let other systems perform these actions.
+ *
+ * The default implementation of the methods does nothing. Override the relevant
+ * methods.
  */
 interface InputListener {
     /**
@@ -17,7 +20,7 @@ interface InputListener {
      * @param y the horizontal coordinate in pixels of the touch location
      * (positive is down)
      */
-    fun onTouch(x: Int, y: Int): Unit
+    fun onTouch(x: Int, y: Int) {}
 
     /**
      * Notifies that the user scrolled the screen by the given amount.
@@ -25,12 +28,12 @@ interface InputListener {
      * @param dx the vertical scrolling amount in pixels
      * @param dy the horizontal scrolling amount in pixels (positive is down)
      */
-    fun onScroll(dx: Int, dy: Int): Unit
+    fun onScroll(dx: Int, dy: Int) {}
 
     /**
      * Notifies that the user pressed the key with the given code.
      *
      * @param keyCode the code of the pressed key
      */
-    fun onKey(keyCode: Int): Unit
+    fun onKey(keyCode: Int) {}
 }
---
+++ algostorm-test/src/test/kotlin/com/aheidelbacher/algostorm/engine/input/PollingInputListenerTest.kt
@@ -0,0 +1,101 @@
+package com.aheidelbacher.algostorm.engine.input
+
+import com.aheidelbacher.algostorm.test.engine.input.InputDriverMock
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+
+class PollingInputListenerTest {
+    private val inputDriver = InputDriverMock()
+    private val listener = PollingInputListener()
+
+    @Before
+    fun initializeInputDriver() {
+        inputDriver.addListener(listener)
+    }
+
+    @Test
+    fun testPollShouldNotifyAllInputsAndNothingMore() {
+        inputDriver.touch(1, 1)
+        inputDriver.key(2)
+        var touched = false
+        assertTrue(listener.poll(object : InputListener {
+            override fun onTouch(x: Int, y: Int) {
+                touched = true
+            }
+
+            override fun onKey(keyCode: Int) {
+                fail()
+            }
+
+            override fun onScroll(dx: Int, dy: Int) {
+                fail()
+            }
+        }))
+        assertTrue(touched)
+        var keyed = false
+        assertTrue(listener.poll(object : InputListener {
+            override fun onTouch(x: Int, y: Int) {
+                fail()
+            }
+
+            override fun onKey(keyCode: Int) {
+                keyed = true
+            }
+
+            override fun onScroll(dx: Int, dy: Int) {
+                fail()
+            }
+        }))
+        assertTrue(keyed)
+        assertFalse(listener.poll(object : InputListener {
+            override fun onTouch(x: Int, y: Int) {
+                fail()
+            }
+
+            override fun onKey(keyCode: Int) {
+                fail()
+            }
+
+            override fun onScroll(dx: Int, dy: Int) {
+                fail()
+            }
+        }))
+    }
+
+    @Test
+    fun testPollMostRecentShouldNotifyLastAndNothingMore() {
+        inputDriver.touch(1, 1)
+        inputDriver.key(2)
+        var keyed = false
+        listener.pollMostRecent(object : InputListener {
+            override fun onTouch(x: Int, y: Int) {
+                fail()
+            }
+
+            override fun onKey(keyCode: Int) {
+                keyed = true
+            }
+
+            override fun onScroll(dx: Int, dy: Int) {
+                fail()
+            }
+        })
+        assertTrue(keyed)
+        assertFalse(listener.poll(object : InputListener {
+            override fun onTouch(x: Int, y: Int) {
+                fail()
+            }
+
+            override fun onKey(keyCode: Int) {
+                fail()
+            }
+
+            override fun onScroll(dx: Int, dy: Int) {
+                fail()
+            }
+        }))
+    }
+}
